44f9d95f7523e6a0a5c1df195e8d2e503ac48ca3
Benji Altman <benji@altmansoftwaredesign.com>
2021-10-08 22:40:12 -0500

    Documentation

--- SynchronizationContextSwap/SynchronizationContextSwap.cs
+++ SynchronizationContextSwap/SynchronizationContextSwap.cs
@@ -1,18 +1,33 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace SynchronizationContextSwap
 {
-	public class SynchronizationContextSwap : IDisposable
+	/// <summary>
+	/// Use within using statment to swap to new context.
+	/// Lets you use <see cref="Task.Wait()"/> or <see cref="Task{T}.Result"/> within the using block.
+	/// </summary>
+	public class SynchronizationContextSwap<T> : IDisposable
 	{
+		/// <summary>
+		/// The old context that is replaced.
+		/// </summary>
 		public SynchronizationContext OldContext { get; private set; }
 
-		SynchronizationContextSwap(SynchronizationContext newContext = null)
+		/// <summary>
+		/// Constructor. Passing nothing is default behavior.
+		/// </summary>
+		/// <param name="newContext">The new SynchronizationContext being passed in. Leave <see langword="null"/> for default behavior.</param>
+		public SynchronizationContextSwap(SynchronizationContext newContext = null)
 		{
 			OldContext = SynchronizationContext.Current;
 			SynchronizationContext.SetSynchronizationContext(newContext);
 		}
 
+		/// <summary>
+		/// Returns to the original context.
+		/// </summary>
 		public void Dispose()
 		{
 			SynchronizationContext.SetSynchronizationContext(OldContext);
